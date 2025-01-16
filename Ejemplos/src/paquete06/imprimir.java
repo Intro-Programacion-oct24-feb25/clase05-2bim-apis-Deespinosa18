e24502a9219cb7a1b3c2e452b37db7eef99036aa
Diego Espinosa <diegoeespinosag18@gmail.com>
2025-01-16 11:41:20 -0500

    cambios importaciones

---
+++ Ejemplos/src/paquete06/imprimir.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete06;
+
+import paquete05.*;
+
+/**
+ *
+ * @author reroes
+ */
+public class imprimir {
+
+    public static int obtenerMultiplicacion(int d, int j) {
+        return d * j;
+    }
+}
